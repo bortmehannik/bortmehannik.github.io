65005110a98c158ebfe3297d8831529802d273ca
bortmehannik <paketonos2@gmail.com>
2021-05-14 13:24:28 +0300

    Add files via upload

---
+++ precache-manifest.225c66f0f32f13fb76de1ecfe6d0cd0f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c598e8780fe9d37353b",
+    "url": "/css/app.e6494143.css"
+  },
+  {
+    "revision": "e67b5840682fbd0960ce",
+    "url": "/css/chunk-452630a2.fc4ddd30.css"
+  },
+  {
+    "revision": "d6ea53fa7a0dbc8d1658ae87f7123ce2",
+    "url": "/img/upload.d6ea53fa.jpg"
+  },
+  {
+    "revision": "3fa859185ec1c8c419099c52cad71da4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c598e8780fe9d37353b",
+    "url": "/js/app.b66b1019.js"
+  },
+  {
+    "revision": "e67b5840682fbd0960ce",
+    "url": "/js/chunk-452630a2.a0b1b33a.js"
+  },
+  {
+    "revision": "90846bd069b5161637fd",
+    "url": "/js/chunk-vendors.b88ec44a.js"
+  },
+  {
+    "revision": "651fba47e9ff8465e4ca7308f469cbfa",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
